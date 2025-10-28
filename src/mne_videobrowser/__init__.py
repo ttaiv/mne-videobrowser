e86a73204c0ee4604aa14ed57fdffb8964009bba
Teemu Taivainen <teemu.taivainen@hotmail.fi>
2025-10-28 10:35:54 +0200

    Update package imports

--- src/mne_videobrowser/__init__.py
+++ src/mne_videobrowser/__init__.py
@@ -1,8 +1,8 @@
 """MNE-Python extension for synchronized viewing of MEG/EEG, video, and audio."""
 
-from .comp_tstamps import comp_tstamps
 from .media.audio import AudioFileHelsinkiVideoMEG
 from .media.video import VideoFileCV2, VideoFileHelsinkiVideoMEG
+from .raw_timestamp_computaton import compute_raw_timestamps
 from .synced_raw_media_browser import (
     browse_raw_with_audio,
     browse_raw_with_video,
@@ -15,7 +15,7 @@ __all__ = [
     "browse_raw_with_audio",
     "browse_raw_with_video_and_audio",
     "TimestampAligner",
-    "comp_tstamps",
+    "compute_raw_timestamps",
     "VideoFileHelsinkiVideoMEG",
     "VideoFileCV2",
     "AudioFileHelsinkiVideoMEG",
