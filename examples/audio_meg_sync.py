99b843879905c12e131586e9d40b35bc27272260
Teemu Taivainen <ttaiv@outlook.com>
2026-07-10 16:26:48 +0300

    Add note about OpenGL to example

--- examples/audio_meg_sync.py
+++ examples/audio_meg_sync.py
@@ -60,6 +60,8 @@ def main() -> None:
         )
 
         # Start the synced raw and audio browsers.
+        # NOTE: Whether you should use OpenGL depends on your system and hardware.
+        # Try both True and False to see which option performs best on your setup.
         raw_browser = raw.plot(block=False, show=False, use_opengl=True)
         browse_raw_with_audio(raw_browser, raw, audio, aligner)
 
